048aa0fb0d3286cc9a96c06487a3e66ff832d2f0
Nelson Rossi Bittencourt <70649161+NelsonBittencourt@users.noreply.github.com>
2021-06-10 00:23:23 -0300

    Add files via upload

--- obj/Debug/net5.0/csVazEdit.AssemblyInfo.cs
+++ obj/Debug/net5.0/csVazEdit.AssemblyInfo.cs
@@ -13,14 +13,15 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("NRB Consultoria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyDescriptionAttribute("\r\n    O csVazEdit é um aplicativo para converter arquivos de vazões binárias em f" +
-    "ormato texto e vice-versa.\r\n    Além de reproduzir as funções do aplicativo orig" +
-    "inal \'VazEdit\' do ONS, o csVazEdit também poderá estendê-las.\r\n    ")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("0.2.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.002")]
+[assembly: System.Reflection.AssemblyDescriptionAttribute("\r\n    O csVazEdit é um aplicativo para converter arquivos binários de vazões, pos" +
+    "tos e MLTs em formato texto e vice-versa.\r\n    Além de reproduzir as funções do " +
+    "aplicativo original \'VazEdit\' do ONS, o csVazEdit também poderá estendê-las.\r\n  " +
+    "  ")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("0.3.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.003")]
 [assembly: System.Reflection.AssemblyProductAttribute("csVazEdit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csVazEdit")]
-[assembly: System.Reflection.AssemblyVersionAttribute("0.2.0.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("0.3.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
 
